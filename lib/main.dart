a65efaf94c98b7f8f297aa370701041febbd0f51
Chris <christianmoses@gmx.de>
2020-09-21 21:00:50 +0200

    MASTER - 21.09.20

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:bubble/bubble.dart';
 import 'package:emoji_picker/emoji_picker.dart';
+import 'package:flutter/services.dart';
+import 'package:share/share.dart';
+import 'package:flutter_link_preview/flutter_link_preview.dart';
+import 'package:http/http.dart' as http;
+import 'package:metadata_fetch/metadata_fetch.dart';
 
 void main() => runApp(MyApp());
 
@@ -41,6 +48,12 @@ class _RandomWordsState extends State<RandomWords> {
   bool _showEmojis = false;
   double _emojiPickerHeight = 0;
   final _markedMessages = List<int>();
+  double _previewHeight = 0;
+  RegExp regExp = new RegExp(
+    r"_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS",
+    caseSensitive: false,
+    multiLine: false,
+  );
 
   Widget _buildSuggestions() {
     return ListView.builder(
@@ -113,25 +126,80 @@ class _RandomWordsState extends State<RandomWords> {
     );
   }
 
+  _superTest(String chatText, int index) {
+    RegExp regExp = new RegExp(
+      r"((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)",
+      caseSensitive: false,
+      multiLine: false,
+    );
+
+    if (regExp.hasMatch(chatText)) {
+      return FlutterLinkPreview(
+        url: chatText,
+        titleStyle: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),
+      );
+    } else {
+      return Text(_chatHistory[index]);
+    }
+  }
+
+  void _getHttpTest() async {
+    try {
+      var data = await extract(
+          "https://strato.de/"); // Use the extract() function to fetch data from the url
+      print(data.image);
+    } catch (e){
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.add_alert),
-            tooltip: 'Show Snackbar',
-
+            icon: const Icon(Icons.content_copy),
+            tooltip: 'Copy',
+            onPressed: () {
+              String copyString = "";
+              _markedMessages.forEach(
+                  (i) => copyString = copyString + _chatHistory[i] + " ");
+              Clipboard.setData(ClipboardData(text: copyString));
+              setState(() {
+                _markedMessages.clear();
+              });
+            },
           ),
           IconButton(
-            icon: const Icon(Icons.navigate_next),
-            tooltip: 'Next page',
-
+            icon: const Icon(Icons.share),
+            tooltip: 'Share',
+            onPressed: () {
+              String shareString = "";
+              _markedMessages.forEach(
+                  (i) => shareString = shareString + _chatHistory[i] + " ");
+              Share.share(shareString);
+              setState(() {
+                _markedMessages.clear();
+              });
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.clear),
+            tooltip: 'Copy',
+            onPressed: () {
+              setState(() {
+                _markedMessages.clear();
+                _chatHistory.clear();
+              });
+            },
           ),
         ],
         title: Text('Chatty'),
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
@@ -139,12 +207,12 @@ class _RandomWordsState extends State<RandomWords> {
             child: ListView.builder(
               itemBuilder: (BuildContext context, int index) {
                 Color color = Colors.white;
-                if (_markedMessages.contains(index)){
+                if (_markedMessages.contains(index)) {
                   color = Colors.yellow;
                 }
                 return GestureDetector(
                   onLongPress: () {
-                    if(_markedMessages.contains(index)){
+                    if (_markedMessages.contains(index)) {
                       setState(() {
                         _markedMessages.remove(index);
                       });
@@ -153,16 +221,13 @@ class _RandomWordsState extends State<RandomWords> {
                         _markedMessages.add(index);
                       });
                     }
-                    },
+                  },
                   child: Bubble(
                       color: color,
-                      child: Text(_chatHistory[index])),
-                  //_chatHistory
+                      child: _superTest(_chatHistory[index], index)),
+                  //Text(_chatHistory[index]) _chatHistory
                   //    .map((message) => Bubble(child: Text(message)))
                   //    .toList(),
-                  onTap: () => Scaffold
-                      .of(context)
-                      .showSnackBar(SnackBar(content: Text(index.toString()))),
                 );
               },
               itemCount: _chatHistory.length,
@@ -171,55 +236,73 @@ class _RandomWordsState extends State<RandomWords> {
             ),
           ),
           Container(
-            height: 50,
-            color: Colors.green[50],
-            child: Row(children: [
-              Container(
-                  width: 100,
-                  color: Colors.red[50],
-                  child: TextField(
-                    onTap: () {
-                      setState(() {
-                        _emojiPickerHeight = 0;
-                        _showEmojis = false;
-                      });
-                    },
-                    controller: myController,
-                    onSubmitted: (chatText) {
-                      setState(() {
-                        _chatHistory.add(chatText);
-                      });
-                      myController.clear();
-                    },
-                  )),
-              FlatButton(
-                onPressed: () {
-                  setState(() {
-                    _showEmojis = !(_showEmojis);
-                  });
-                  if(_showEmojis == true){
-                    setState(() {
-                      _emojiPickerHeight = 211;
-                    });
-                  } else {
-                    setState(() {
-                      _emojiPickerHeight = 0;
-                    });
-                    }
-                  FocusScopeNode currentFocus = FocusScope.of(context);
+              height: 50 + _previewHeight,
+              color: Colors.green[50],
+              child: Column(
+                children: [
+                  Container(
+                    height: _previewHeight,
+                    child: null
+                  ),
+                  Row(children: [
+                    Container(
+                        width: 100,
+                        color: Colors.red[50],
+                        child: TextField(
+                          onTap: () {
+                            setState(() {
+                              _emojiPickerHeight = 0;
+                              _showEmojis = false;
+                            });
+                          },
+                          controller: myController,
+                          onChanged: (changedString) {
+                            if (regExp.hasMatch(changedString)) {
+                              setState(() {
+                                _previewHeight = 120;
+                              });
+                            } else {
+                              setState(() {
+                                _previewHeight = 0;
+                              });
+                            }
+                          },
+                          onSubmitted: (chatText) {
+                            _getHttpTest();
+                            setState(() {
+                              _chatHistory.insert(0, chatText);
+                            });
+                            myController.clear();
+                          },
+                        )),
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          _showEmojis = !(_showEmojis);
+                        });
+                        if (_showEmojis == true) {
+                          setState(() {
+                            _emojiPickerHeight = 211;
+                          });
+                        } else {
+                          setState(() {
+                            _emojiPickerHeight = 0;
+                          });
+                        }
+                        FocusScopeNode currentFocus = FocusScope.of(context);
 
-                  if (!currentFocus.hasPrimaryFocus) {
-                    currentFocus.unfocus();
-                    setState(() {
-                    });
-                  }
-                },
-                child: Text(
-                  "Emotes",
-                ),
-              )
-            ]),
-          ),
+                        if (!currentFocus.hasPrimaryFocus) {
+                          currentFocus.unfocus();
+                          setState(() {});
+                        }
+                      },
+                      child: Text(
+                        "Emotes",
+                      ),
+                    )
+                  ]),
+                ],
+              )),
           Container(
             height: _emojiPickerHeight,
             child: EmojiPicker(
